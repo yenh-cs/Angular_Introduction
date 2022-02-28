17fe76250b0dd8d7f465be7575d91571fa5bd2c0
Yen Hoang <yen.hoang@contentsquare.com>
2022-02-28 14:38:00 +0100

    add all code

---
+++ src/app/media-item-list.component.ts
@@ -0,0 +1,40 @@
+import { Component, OnInit } from '@angular/core';
+import { MediaItemService, MediaItem } from './media-item.service';
+import { ActivatedRoute } from '@angular/router';
+
+@Component({
+  selector: 'mw-media-item-list',
+  templateUrl: './media-item-list.component.html',
+  styleUrls: ['./media-item-list.component.css']
+})
+export class MediaItemListComponent implements OnInit {
+  medium = '';
+  mediaItems: MediaItem[];
+
+  constructor(private mediaItemService: MediaItemService, private activatedRoute: ActivatedRoute) {}
+
+  ngOnInit() {
+    this.activatedRoute.paramMap.subscribe(paramMap => {
+      let medium: string = paramMap.get('medium');
+      if (medium.toLowerCase() === 'all'){
+        medium = '';
+      }
+      this.getMediaItems(medium);
+    });
+  }
+
+  onMediaItemDelete(mediaItem: MediaItem) {
+    this.mediaItemService.delete(mediaItem)
+      .subscribe(() => {
+        this.getMediaItems(this.medium);
+      });
+  }
+
+  getMediaItems(medium: string) {
+    this.medium = medium;
+    this.mediaItemService.get(medium)
+      .subscribe(mediaItems => {
+        this.mediaItems = mediaItems;
+      });
+  }
+}
